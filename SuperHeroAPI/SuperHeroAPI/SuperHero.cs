9ab3d3641c04bd197df7e9e77b13b77157fee4f9
whackablemole <63235752+whackablemole@users.noreply.github.com>
2022-02-15 22:02:33 +0000

    Corrected case on string.

--- SuperHeroAPI/SuperHeroAPI/SuperHero.cs
+++ SuperHeroAPI/SuperHeroAPI/SuperHero.cs
@@ -3,7 +3,7 @@
     public class SuperHero
     {
         public int Id { get; set; }
-        public string Name { get; set; } = String.Empty;
+        public string Name { get; set; } = string.Empty;
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Place { get; set; } = string.Empty;
